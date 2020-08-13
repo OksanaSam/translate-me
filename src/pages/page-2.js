3ae7b09317186f6813b613365587b5c4fde5212c
oksana sam <oksana.o.samokhvalova@gmail.com>
2020-08-12 21:47:33 -0400

    added react icons🙃

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,38 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+import Image from '../components/image'
+import SEO from '../components/seo'
+import Layout from '../components/layout'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { MdFlightTakeoff, MdImportantDevices } from 'react-icons/md'
+import { DiReact, DiTerminal } from 'react-icons/di';
+import { FaApple, FaAngellist } from 'react-icons/fa';
+import { GiCycling, GiHemp } from 'react-icons/gi';
+import { GoGlobe, GoSquirrel } from  'react-icons/go';
+import { WiDayFog, WiNightHail } from 'react-icons/wi';
 
-const SecondPage = () => (
+
+import { IconContext } from 'react-icons';
+
+
+const AboutPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from About page</h1>
-    <p>Welcome About page</p>
-    <Link to="/">Back to the homepage</Link>
+    <SEO title='Page two' />
+    <h1>About</h1>
+    <p>Welcome to translate-me app!</p>
+    <IconContext.Provider value={{ verticalAlign: 'middle', size: '2rem', color: '#6D309C', className: 'global-class-name' }}>
+      {/* <MdFlightTakeoff/><MdImportantDevices/><br/> */}
+      <DiReact/><DiTerminal/><br/>
+      {/* <FaApple/><FaAngellist/><br/>
+      <GiCycling/> <GiHemp/> <br/>
+      <GoGlobe/><GoSquirrel/><br/>
+      <WiDayFog/><WiNightHail/><br/> */}
+    </IconContext.Provider>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to='/'>Back Home</Link>
   </Layout>
 )
 
-export default SecondPage
+export default AboutPage
